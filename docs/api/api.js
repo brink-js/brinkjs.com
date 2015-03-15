4086ecf7b3ba3273bb2a3a60e36a9e8c6e74babf
Taka Kojima <taka@gigafied.com>
2015-03-14 22:31:19 -0700

    Add docs

---
+++ docs/api/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Brink",
+        "Brink.Class",
+        "Brink.CoreObject",
+        "Brink.Object"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
